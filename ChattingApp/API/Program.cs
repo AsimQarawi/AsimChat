59d41d148867f2d237794e322203744dacde5add
AsimQarawi <asimqarawy@gmail.com>
2024-04-29 16:29:33 +0300

    add bootstap/https to angular/ make a get request to the server/ add cors header to the programfile

--- ChattingApp/API/Program.cs
+++ ChattingApp/API/Program.cs
@@ -13,9 +13,11 @@ builder.Services.AddDbContext<DataContext>(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
